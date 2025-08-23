7e6ad10865d15fc5dcbda3dd5fa3296cfa27f237
Max <87113767+Bttlbmb@users.noreply.github.com>
2025-08-23 19:32:38 +0200

    Create pelicanconf.py

---
+++ pelicanconf.py
@@ -0,0 +1,10 @@
+AUTHOR = "Max"
+SITENAME = "Hidden Gem Games"
+SITEURL = ""                 # set only in publishconf.py
+
+PATH = "content"
+TIMEZONE = "Europe/Berlin"
+DEFAULT_LANG = "en"
+THEME = "notmyidea"          # built-in, simple
+
+DEFAULT_PAGINATION = 10
