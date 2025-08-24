3a74c9989d471777ce83c8af8411624b2f424666
Max <87113767+Bttlbmb@users.noreply.github.com>
2025-08-24 22:02:34 +0200

    Update publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -1,5 +1,8 @@
 from pelicanconf import *
 
+from datetime import datetime
+CURRENTYEAR = datetime.now().year
+
 # replace <USER> and <REPO> below
 SITEURL = "https://Bttlbmb.github.io/hiddengemgames"
 RELATIVE_URLS = False
