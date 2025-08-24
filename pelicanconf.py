beac8c6ef59998057cb761ddb8bd6ae11173bcdc
Max <87113767+Bttlbmb@users.noreply.github.com>
2025-08-24 12:10:40 +0200

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,11 @@ SITENAME = "Hidden Gem Games"
 SITEURL = ""                 # set only in publishconf.py
 
 PATH = "content"
+ARTICLE_PATHS = ["", "posts"]
+
 TIMEZONE = "Europe/Berlin"
 DEFAULT_LANG = "en"
 THEME = "notmyidea"          # built-in, simple
 
 DEFAULT_PAGINATION = 10
+
